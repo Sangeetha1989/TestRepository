bbfd47e923cae166a21061aebd7a6d3c4c81ef8f
Sangeetha1989 <45203056+Sangeetha1989@users.noreply.github.com>
2018-11-23 15:11:01 +0530

    MVC_EF

---
+++ TwitterCloneMVC/TwitterCloneMVC/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace TwitterCloneMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Person login)
+        {
+            if (ModelState.IsValid)
+            {
+                TwitterCloneEntities db = new TwitterCloneEntities();
+                var user = (from userlist in db.People
+                            where userlist.User_id == login.User_id && userlist.password == login.password
+                            select new
+                            {
+                                userlist.User_id,
+                                userlist.password
+                            }).ToList();
+                if (user.FirstOrDefault() != null)
+                {
+                    Session["UserName"] = user.FirstOrDefault().User_id;
+                    Session["Password"] = user.FirstOrDefault().password;
+                    //string username = User.Identity.Name= user.FirstOrDefault().User_id;
+                    return RedirectToAction("Index", "TWEETs");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login credentials.");
+                }
+            }
+            return View(login);
+        }
+
+
+
+        [AllowAnonymous]
+        public ActionResult SignUp()
+        {
+            //ViewBag.Message = "Your application description page.";
+
+            return this.View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult SignUp(Person obj)
+        {
+            //ViewBag.Message = "Your application description page.";
+            TwitterCloneEntities twitEntityObj = new TwitterCloneEntities();
+            twitEntityObj.People.Add(obj);
+            twitEntityObj.SaveChanges();
+            return this.View();
+        }
+        public ActionResult userProfile()
+        {
+            string userName = Session["UserName"].ToString();
+            TwitterCloneEntities twitEntityObj = new TwitterCloneEntities();
+            var userObj = twitEntityObj.People.Where(s => s.User_id == userName).FirstOrDefault();
+            return View(userObj);
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult userProfile(Person obj)
+        {
+            //ViewBag.Message = "Your application description page.";
+            TwitterCloneEntities twitEntityObj = new TwitterCloneEntities();
+            //twitEntityObj.People.Add(obj);
+
+            var userObj = twitEntityObj.People.FirstOrDefault(u => u.User_id.Equals(obj.User_id));
+
+            // Update fields
+            userObj.User_id = obj.User_id;
+            userObj.fullName = obj.fullName;
+            userObj.email = obj.email;
+            userObj.active = obj.active;
+            userObj.joined = obj.joined;
+
+            twitEntityObj.Entry(userObj).State = EntityState.Modified;
+            twitEntityObj.SaveChanges();
+            return this.View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return this.RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ TwitterCloneMVC/TwitterCloneMVC/Controllers/TWEETsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TwitterCloneMVC;
+using System.Linq;
+
+namespace TwitterCloneMVC.Views.Tweets
+{
+    public class TWEETsController : Controller
+    {
+        private TwitterCloneEntities db = new TwitterCloneEntities();
+
+        // GET: TWEETs
+        public ActionResult Index()
+        {
+            var tWEETs = db.TWEETs.Include(t => t.Person);
+            //tWEETs = from x in tWEETs where x.user_id == Session["UserName"].ToString() select x;
+            return View(tWEETs.ToList());
+
+        }
+
+        // GET: TWEETs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TWEET tWEET = db.TWEETs.Find(id);
+            if (tWEET == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tWEET);
+        }
+
+        // GET: TWEETs/Create
+        public ActionResult Create()
+        {
+            ViewBag.user_id = new SelectList(db.People, "User_id", "password");
+            return View();
+        }
+
+        // POST: TWEETs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "user_id,message,created")] TWEET tWEET)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TWEETs.Add(tWEET);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_id = new SelectList(db.People, "User_id", "password", tWEET.user_id);
+            return View(tWEET);
+        }
+
+        // GET: TWEETs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TWEET tWEET = db.TWEETs.Find(id);
+            if (tWEET == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_id = new SelectList(db.People, "User_id", "password", tWEET.user_id);
+            return View(tWEET);
+        }
+
+        // POST: TWEETs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "tweet_id,user_id,message,created")] TWEET tWEET)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tWEET).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_id = new SelectList(db.People, "User_id", "password", tWEET.user_id);
+            return View(tWEET);
+        }
+
+        // GET: TWEETs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TWEET tWEET = db.TWEETs.Find(id);
+            if (tWEET == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tWEET);
+        }
+
+        // POST: TWEETs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TWEET tWEET = db.TWEETs.Find(id);
+            db.TWEETs.Remove(tWEET);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
