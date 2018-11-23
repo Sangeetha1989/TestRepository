92da90c1085f63ff60743457bcfc22db8d161727
Sangeetha1989 <45203056+Sangeetha1989@users.noreply.github.com>
2018-11-23 11:21:39 +0530

    WCF Test Commit
    WCF Test Commit

---
+++ 21 - WCF Assignment - 1/21 - WCF Assignment - 1/IService1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace _21___WCF_Assignment___1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculator
+    {
+
+        [OperationContract]
+        int DoAdd(int value1,int value2);
+
+        [OperationContract]
+        int DoSubtract(int value1, int value2);
+
+       
+    }    
+}
---
+++ 21 - WCF Assignment - 1/21 - WCF Assignment - 1/Service Consumer/ServiceConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceConsumer.CalculatorService;
+
+namespace ServiceConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClient proxy = new CalculatorClient();
+            int number1 = 0;
+            int number2 = 0;
+            Console.WriteLine("Welcome User!!! \n");
+            Console.WriteLine("Enter First Number : ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number : ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition Result : {0}", proxy.DoAdd(number1, number2));
+            Console.WriteLine("Subtraction Result : {0}", proxy.DoSubtract(number1, number2));           
+            Console.ReadLine();
+        }
+    }
+}
