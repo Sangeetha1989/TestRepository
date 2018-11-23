50d77039a717f087325fb7ef1eb954be3c261f3a
Sangeetha1989 <45203056+Sangeetha1989@users.noreply.github.com>
2018-11-23 14:52:56 +0530

    Add files via upload

---
+++ 21_WCF Assigment 1_Task2/21_WCF Assignment 1_Task2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _21_WCF_Assignment_1_Task2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string btnclickVal)
+        {
+            if (btnclickVal == "HTTPButton")
+            {
+                ServiceReference1.Service1Client Objhttp = new ServiceReference1.Service1Client("ephttp");
+                Objhttp.Open();
+                List<string> GetOpenings = Objhttp.GetJobOpenings().ToList();
+                ViewBag.GetOpening = GetOpenings;                
+                List<string> GetOpeningsByRole = Objhttp.GetJobOpeningsByRole("LEAD").ToList();
+                ViewBag.GetOpeningByRole = GetOpeningsByRole;
+                Objhttp.Close();
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
